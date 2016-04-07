5b1e20fb969ddee298dd3d6cab9b0f14bd97f285
Risha <rishallen@gmail.com>
2016-04-06 18:11:29 -0700

    completed the basline and worked on the ouptut message

---
+++ scrabble.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+require_relative 'lib/scoring'
+
+
+class ScrabbleSinatra < Sinatra::Base
+
+get '/' do
+  erb :index
+end
+
+get '/score' do
+  erb :score
+end
+
+post '/score' do # this defines a route, any requests that come in that "post" this is the block of code that will happen
+  # peep is the name of the key in the hash that is located in the my_first_form file
+  @wordscore = Scrabble::Scoring.score(params["word"].upcase) # this instantiates a new form
+  @word = params["word"]
+  erb :score
+end
+
+  run!
+end
+s

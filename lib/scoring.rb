b3a8f77aa9c7d41238427828f62470c7e5f99484
Risha <rishallen@gmail.com>
2016-04-07 15:08:33 -0700

    completed primary requirments for sinatra scrabble

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -1,60 +1,69 @@
 module Scrabble
 
-class Scoring
+  class Scoring
 
-SEVEN_LETTER_BONUS = 50
-#PLAYED_WORDS = []
-#WORD_SCORE_COLLECTION =[]
+  SEVEN_LETTER_BONUS = 50
+  #PLAYED_WORDS = []
+  #WORD_SCORE_COLLECTION =[]
 
-  LETTERS = {
-  %w(A E I O U L N R S T) => 1,
-  %w(D G) => 2,
-  %w(B C M P) => 3,
-  %w(F H V W Y) => 4,
-  %w(K) => 5,
-  %w(J X) => 8,
-  %w(Q Z) => 10
-}
+    LETTERS = {
+    %w(A E I O U L N R S T) => 1,
+    %w(D G) => 2,
+    %w(B C M P) => 3,
+    %w(F H V W Y) => 4,
+    %w(K) => 5,
+    %w(J X) => 8,
+    %w(Q Z) => 10
+  }
 
-  def self.letter_value(letter)
-    LETTERS.each do |key, value|
-      if key.include? letter
-        return value
+    def self.letter_value(letter)
+      LETTERS.each do |key, value|
+        if key.include? letter
+          return value
+        end
+      end
+      return -100000 # this will allow me to know if there is a number that isn't in the LETTERS hash
+    end
+
+    def self.score(word)
+      word = word.upcase 
+      word_score = 0
+      bonus = SEVEN_LETTER_BONUS
+      word_score += bonus if word.length == 7
+      word_array = word.split("")
+      word_array.each do |letter|
+        temp_letter_val = letter_value(letter)
+        word_score += temp_letter_val
+      end
+      return word_score
+    end
+
+  #return highest score from array_of_tiles
+  #in case of tie :
+  #      -better to use fewer tiles
+  #      -if top scores is between multiple words and one with 7, one using 7 win_words
+  #      - multiple words with same score and same length, pick first
+    def self.highest_score_from(word_array)
+      by_scores = word_array.group_by { |word| self.score(word) }
+        win_words = by_scores.max[1]
+      if win_words.length == 1
+        return win_words[0]
+      elsif win_words.any? { |word| word.length == 7 }
+          seven_letter_words = win_words.select { |word| word.length == 7}
+          return seven_letter_words.first
+      else
+        min_word = win_words.min_by { |word| word.length }
+        return min_word
       end
     end
-    return -100000 # this will allow me to know if there is a number that isn't in the LETTERS hash
-  end
 
-  def self.score(word)
-    word_score = 0
-    bonus = SEVEN_LETTER_BONUS
-    word_score += bonus if word.length == 7
-    word_array = word.split("")
-    word_array.each do |letter|
-      temp_letter_val = letter_value(letter)
-      word_score += temp_letter_val
-    end
-    return word_score
-  end
-
-#return highest score from array_of_tiles
-#in case of tie :
-#      -better to use fewer tiles
-#      -if top scores is between multiple words and one with 7, one using 7 win_words
-#      - multiple words with same score and same length, pick first
-  def self.highest_score_from(word_array)
-    by_scores = word_array.group_by { |word| self.score(word) }
-      win_words = by_scores.max[1]
-    if win_words.length == 1
-      return win_words[0]
-    elsif win_words.any? { |word| word.length == 7 }
-        seven_letter_words = win_words.select { |word| word.length == 7}
-        return seven_letter_words.first
-    else
-      min_word = win_words.min_by { |word| word.length }
-      return min_word
+    def self.many_words(words)
+      words_hash = {}
+      word_split = words.split(" ")
+      word_split.each do |word|
+        words_hash[word] = self.score(word) # this is where the key value pair happens
+        end
+      return words_hash
     end
   end
-
-end
 end
--- scrabble.rb
+++ scrabble.rb
@@ -4,20 +4,30 @@ require_relative 'lib/scoring'
 
 class ScrabbleSinatra < Sinatra::Base
 
-get '/' do
-  erb :index
-end
+  get '/' do
+    erb :index
+  end
 
-get '/score' do
-  erb :score
-end
+  get '/score' do
+    erb :score
+  end
 
-post '/score' do # this defines a route, any requests that come in that "post" this is the block of code that will happen
-  # peep is the name of the key in the hash that is located in the my_first_form file
-  @wordscore = Scrabble::Scoring.score(params["word"].upcase) # this instantiates a new form
-  @word = params["word"]
-  erb :score
-end
+  post '/score' do # this defines a route, any requests that come in that "post" this is the block of code that will happen
+    # peep is the name of the key in the hash that is located in the my_first_form file
+    @wordscore = Scrabble::Scoring.score(params["word"]) # this instantiates a new form
+    @word = params["word"]
+    erb :score
+  end
+
+  get '/score_many' do
+    erb :score_many
+  end
+
+  post '/score_many' do
+    @collection_of_words = Scrabble::Scoring.many_words(params["words"])
+    @words = params["words"]
+  erb :score_many
+  end
 
   run!
 end
